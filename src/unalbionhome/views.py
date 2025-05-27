f4cbb6c46b1470ab7ae0162e18e87832c73e5c87
Attreya <attreya01@gmail.com>
2025-05-27 12:21:27 +0530

    first commit

---
+++ src/unalbionhome/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+def home_view(request, *args, **kwargs):
+    context = {
+        'title': 'UnAlbion Home',
+        'description': 'Welcome to UnAlbion, your go-to place for all things Albion Online.',
+        'keywords': 'Albion Online, MMORPG, gaming, community',
+    }
+    return render(request, 'home.html', context)
