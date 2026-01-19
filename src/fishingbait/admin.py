abf9697f3873d4ab021d32aea8d8b0b7d63ab93f
Attreya <attreya01@gmail.com>
2026-01-19 22:01:34 +0530

    added aminal panel access to fishing bait data

--- src/fishingbait/admin.py
+++ src/fishingbait/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import FishingBaitData
 
-# Register your models here.
+
+@admin.register(FishingBaitData)
+class FishingBaitDataAdmin(admin.ModelAdmin):
+    list_display = ('user', 'worm_price', 't1_price', 't3_price', 't5_price')
+    list_filter = ('user',)
+    search_fields = ('user__username',)
